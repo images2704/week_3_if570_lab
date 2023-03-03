1b1e41b2c40e871cf5489698562474696a5fa61a
Stefan Aprilio <45485778+images2704@users.noreply.github.com>
2023-03-03 09:34:01 +0700

    Week03

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+package example.myapp
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun buildAquarium() {
+    val myAquarium = Aquarium().also {
+        it.printSize()
+    }
+}
+
+fun main() {
+    buildAquarium()
 }
---
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -0,0 +1,15 @@
+package example.myapp
+
+import com.sun.tools.classfile.Module_attribute.ProvidesEntry.length
+
+class Aquarium {
+    var width: Int = 20
+    var height: Int = 40
+    var length: Int = 100
+}
+
+fun printSize() {
+    val width = ""
+    val height = ""
+    println("Width: $width cm " + "Length: $length cm" + "Height: $height cm")
+}
